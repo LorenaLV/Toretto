ec753bb8708a95dc766dfd2885f1d4244b7082fe
Mauricio Quijada <majiqtresi@gmail.com>
2016-10-19 19:45:13 -0500

    Editando conectar.sh

--- conectar.sh
+++ conectar.sh
@@ -1,3 +1,4 @@
-# Asegurese de primero conectarse a la red ROS
+echo " Asegurese de primero conectarse a la red ROS"
+echo "conectando"
 export ROS_MASTER_URI=http://192.168.43.102:11311
 export ROS_HOSTNAME=192.168.43.106
